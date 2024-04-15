f6b1030ec9d69c563f6984a221fdda68bf667f06
Leonardo Cutrim <141735210+yCarpal@users.noreply.github.com>
2024-04-15 14:43:34 +0000

    bd 15/04

---
+++ bd.aula_dia 15/04/bd.sql
@@ -0,0 +1,30 @@
+CREATE DOMAIN d_nomeProprio VARCHAR(40);
+CREATE DOMAIN d_telefone VARCHAR(15);
+CREATE DOMAIN d_moedas DECIMAL(15,2);
+
+
+CREATE TABLE ingredientes(
+    idIngrediente SERIAL PRIMARY KEY NOT NULL,
+    unidadeMedida VARCHAR (10) NOT NULL,
+    ingrediente VARCHAR (20) NOT NULL
+    );
+
+
+CREATE TABLE pizzarias(
+    idPizzaria SERIAL PRIMARY KEY NOT NULL,
+    nome d_nomeProprio NOT NULL,
+    telefone d_telefone NOT NULL,
+    endereco VARCHAR (50) NOT NULL
+);
+
+
+-- tabelas secundarias, possui atritubutos normais porém tem um relacionamento de dependencia com outra tabela, 
+
+
+CREATE TABLE cardapios(
+    idCardapio SERIAL PRIMARY KEY NOT NULL,
+    descricao VARCHAR(15) NOT NULL,
+    idPizzaria INTEGER NOT NULL, --FK
+    FOREING KEY (idPizzaria) REFERENCES pizzarias(idPizzaria)
+)
+
